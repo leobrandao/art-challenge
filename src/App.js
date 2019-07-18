446d4def189938e78090fb17545ff32bd241dad0
Leonardo Brandao <Leonardo_Brandao@gap.com>
2019-07-18 01:39:53 -0700

    Updating state model

--- src/App.js
+++ src/App.js
@@ -7,44 +7,44 @@ import Column from "./components/Column";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    //   columns: [
+    //     {
+    //       name: "Person 1",
+    //       items: ["Item 1", "Item 2", "Item 3"]
+    //     },
+    //     {
+    //       name: "Person 2",
+    //       items: ["Item 4", "Item 5", "Item 6"]
+    //     },
+    //     {
+    //       name: "Person 3",
+    //       items: ["Item 7", "Item 8", "Item 9"]
+    //     },
+    //     {
+    //       name: "Person 4",
+    //       items: ["Item 10", "Item 11", "Item 12"]
+    //     }
+    //   ]
+    // };
+
     this.state = {
-      columns: [
-        {
-          name: "Person 1",
-          items: ["Item 1", "Item 2", "Item 3"]
-        },
-        {
-          name: "Person 2",
-          items: ["Item 4", "Item 5", "Item 6"]
-        },
-        {
-          name: "Person 3",
-          items: ["Item 7", "Item 8", "Item 9"]
-        },
-        {
-          name: "Person 4",
-          items: ["Item 10", "Item 11", "Item 12"]
-        }
-      ]
-    };
+      "person 1": ["Item 1", "Item 2", "Item 3"],
+      "person 2": [ "Item 4", "Item 5", "Item 6" ],
+      "Person 3": [ "Item 7", "Item 8", "Item 9" ],
+      "Person 4": [ "Item 10", "Item 11", "Item 12" ]
+    }
 
-    
   }
-
 
   addNewItem = columnName => {
     let newItem = window.prompt("Enter New Item to " + columnName);
-    console.log(newItem);
     if (newItem === null || newItem === '') return;
-    const columns = this.state.columns.map(column => {
-      if (column.name === columnName) {
-        column.items.push(newItem);
-      }
-      return column;
-    });
-    this.setState({
-      columns
-    });
+    const columns = { ...this.state }
+    
+    columns[columnName] = [...columns[columnName], newItem];
+
+    this.setState( columns );
   };
 
   moveItem = (item, columnName, direction) => {
@@ -68,18 +68,19 @@ class App extends React.Component {
   }
 
   render() {
-    const columns = this.state.columns;
+    const columns = this.state;
+
     return (
       <div className="container">
         <h1 className="app--title">ART.com Kanban Challenge</h1>
         <div className="row">
-          {columns.map((column, i) => (
+          {Object.entries(columns).map(([key, value]) => (
             <Column
-              key={i}
+              key={key}
               addNewItem={this.addNewItem}
               moveItem={this.moveItem}
-              columnName={column.name}
-              items={column.items}
+              columnName={key}
+              items={value}
             />
           ))}
         </div>
