c3cf99bb5122c53d7697d84a08042c0291e22800
Leonardo Brandao <leonardolcbrandao@gmail.com>
2019-07-22 14:24:36 -0700

    Updates

---
+++ precache-manifest.e9de6e50f9a4d7599a142c854d743e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1060bd5f9c9209bce256d2e13aceb82",
+    "url": "/kanban-challenge/index.html"
+  },
+  {
+    "revision": "94020246bf8b9cf6c353",
+    "url": "/kanban-challenge/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5fbfdf4e496722d029b8",
+    "url": "/kanban-challenge/static/css/main.b43e117e.chunk.css"
+  },
+  {
+    "revision": "94020246bf8b9cf6c353",
+    "url": "/kanban-challenge/static/js/2.3567cc7e.chunk.js"
+  },
+  {
+    "revision": "5fbfdf4e496722d029b8",
+    "url": "/kanban-challenge/static/js/main.d5a5e7de.chunk.js"
+  },
+  {
+    "revision": "2adefc5892e07d01cf5c",
+    "url": "/kanban-challenge/static/js/runtime~main.6320d26f.js"
+  }
+]);
